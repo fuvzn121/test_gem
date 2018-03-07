c6071800b0751e6a890d7c7c0c3c2c737184f03e
Kouhei Shimamura <shimamura@appirits.com>
2018-03-07 14:28:40 +0900

    gem test

--- spec/test_gem_spec.rb
+++ spec/test_gem_spec.rb
@@ -1,9 +1,13 @@
-RSpec.describe TestGem do
-  it "has a version number" do
-    expect(TestGem::VERSION).not_to be nil
+require 'spec_helper'
+
+describe TestGem do
+  it 'has a version number' do
+    expect(TestGem::VERSION.not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#greet' do
+    it 'returns "Hello World!"' do
+      expect(TestGem.greet).to eq('Hello World!')
+    end
   end
 end
--- test_gem.gemspec
+++ test_gem.gemspec
@@ -11,7 +11,7 @@ Gem::Specification.new do |spec|
 
   spec.summary       = %q{test summary}
   spec.description   = %q{test description}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.homepage      = "https://github.com/fuvzn121/test_gem"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
